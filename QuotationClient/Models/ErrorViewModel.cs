89ff26a2e9c294a6447f85f413ae0134b618a844
Kim Ta <hoangkim.lol2002@gmail.com>
2024-03-03 17:32:52 +0700

    delete

--- QuotationClient/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace QuotationClient.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
