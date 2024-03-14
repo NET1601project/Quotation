ec5441a849f89da85deba20bef2a62970c1491ef
Minhthong09 <minhthongpham9a2@gmail.com>
2024-03-14 13:06:59 +0700

    coommit

--- Application/Migrations/20240312061451_InitialCreate.Designer.cs
+++ Application/Migrations/20240312061451_InitialCreate.Designer.cs
@@ -12,7 +12,7 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Application.Migrations
 {
     [DbContext(typeof(AppDBContext))]
-    [Migration("20240308044054_InitialCreate")]
+    [Migration("20240312061451_InitialCreate")]
     partial class InitialCreate
     {
         protected override void BuildTargetModel(ModelBuilder modelBuilder)
